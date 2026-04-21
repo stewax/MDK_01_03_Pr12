96ae2e011eff50764ad5716c12ebc7ed25681a37
Khodyrev Nikita <sigma29034@gmail.com>
2026-04-21 16:32:08 +0500

    Модель данных Осенников

---
+++ network/src/main/java/com/example/network/domains/common/Settings.java
@@ -0,0 +1,6 @@
+package com.example.network.domains.common;
+
+public class Settings {
+    public static String URL = "http:/10.0.2.2:5000";
+    public static String DEMO_TOKEN = "233f2be4-6f04-426d-bc70-ecf66a6f8d4d";
+}
