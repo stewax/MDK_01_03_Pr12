993566ddef9d456df5924d21a5cb37c81309a00b
Khodyrev Nikita <sigma29034@gmail.com>
2026-04-21 16:25:13 +0500

    Интерфейс Осенников

---
+++ network/src/main/java/com/example/network/domains/callbacks/MyResponseCallbacks.java
@@ -0,0 +1,6 @@
+package com.example.network.domains.callbacks;
+
+public interface MyResponseCallbacks {
+    void onCompile(String result);
+    void onError(String error);
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,4 +21,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "Pr12"
 include(":app")
- 
+include(":network")
