d1cb9dcea77b0282b5db4f97b5395f47ea739eff
Khodyrev Nikita <sigma29034@gmail.com>
2026-04-21 16:35:47 +0500

    класс авторизации Осенников

---
+++ network/src/main/java/com/example/network/datas/users/UserLogin.java
@@ -0,0 +1,42 @@
+package com.example.network.datas.users;
+
+import com.example.network.domains.apis.MyAsyncTask;
+import com.example.network.domains.callbacks.MyResponseCallbacks;
+import com.example.network.domains.common.Settings;
+import com.example.network.domains.models.User;
+import com.google.gson.GsonBuilder;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+
+import java.io.IOException;
+
+// 2 usages
+public class UserLogin extends MyAsyncTask {
+    // 2 usages
+    User user;
+    // 1 usage
+    public UserLogin(User user, MyResponseCallbacks callback) {
+        super(callback);
+        this.user = user;
+    }
+
+    @Override
+    protected String doInBackground(Void... voids) {
+        String rawData = new GsonBuilder().create().toJson(user);
+        try {
+            Connection.Response response = Jsoup.connect(Settings.URL + "/api/user/login")
+                    .ignoreContentType(true)
+                    .ignoreHttpErrors(true)
+                    .method(Connection.Method.POST)
+                    .header("Content-type", "application/json")
+                    .requestBody(rawData)
+                    .execute();
+            return response.statusCode() == 200 ?
+                    response.body() :
+                    "Error: " + response.body();
+        } catch (IOException e) {
+            return "Error: " + e.getMessage();
+        }
+    }
+}
